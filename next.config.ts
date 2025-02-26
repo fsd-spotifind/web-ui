4d34f5c87589b8c271d86966c2b37c6f6b9c5e95
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-02-26 23:39:25 +0800

    feat(ui): sotd card molecule

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+        pathname: "/image/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
