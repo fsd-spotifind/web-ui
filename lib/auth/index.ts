6d18458041d366724cbfed61762447425522fe6f
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-03-20 16:20:23 +0800

    chore: update betterauth options

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -3,6 +3,7 @@ import { Pool } from "pg";
 import { env } from "../env";
 
 export const auth = betterAuth({
+  baseURL: env.BETTER_AUTH_URL,
   database: new Pool({
     connectionString: env.DATABASE_URL,
   }),
@@ -10,6 +11,8 @@ export const auth = betterAuth({
     spotify: {
       clientId: env.SPOTIFY_CLIENT_ID,
       clientSecret: env.SPOTIFY_CLIENT_SECRET,
+      redirectURI: `${env.BETTER_AUTH_URL}/api/auth/callback/spotify`,
     },
   },
+  logger: console,
 });
