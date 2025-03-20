d9d1ad76756660b27de9cf1e549e589422381ab1
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-03-20 17:45:41 +0800

    Revert "chore: separate auth POST request handle see https://github.com/better-auth/better-auth/blob/main/demo/nextjs/app/api/auth/%5B...all%5D/route.ts"
    This reverts commit bf405f96f8281950ee0a4bf6a021388c54fcb511.

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,10 +1,4 @@
-import { auth } from "@/lib/auth";
+import { auth } from "@/lib/auth"; // path to your auth file
 import { toNextJsHandler } from "better-auth/next-js";
-import { NextRequest } from "next/server";
 
-export const { GET } = toNextJsHandler(auth);
-
-export const POST = async (req: NextRequest) => {
-  const res = await auth.handler(req);
-  return res;
-};
+export const { POST, GET } = toNextJsHandler(auth);
