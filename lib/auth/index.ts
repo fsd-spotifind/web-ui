d794e773d869f8e80e1cf531a52c7755e75eaa4b
David Lin <13061926+DavidLHW@users.noreply.github.com>
2025-03-20 17:51:09 +0800

    chore: revert to default configurations for betterauth

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -3,7 +3,6 @@ import { Pool } from "pg";
 import { env } from "../env";
 
 export const auth = betterAuth({
-  baseURL: env.BETTER_AUTH_URL,
   database: new Pool({
     connectionString: env.DATABASE_URL,
   }),
@@ -11,13 +10,6 @@ export const auth = betterAuth({
     spotify: {
       clientId: env.SPOTIFY_CLIENT_ID,
       clientSecret: env.SPOTIFY_CLIENT_SECRET,
-      redirectURI: `${env.BETTER_AUTH_URL}/api/auth/callback/spotify`,
     },
   },
-  logger: {
-    disabled: false,
-    level: "debug",
-    log: (_, message, ...rest) => console.log(message, ...rest),
-  },
-  trustedOrigins: [env.BETTER_AUTH_URL],
 });
